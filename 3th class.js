3f227294f04791fb6f0e5fe2d811a598b7c09e23
Musrat Ali <musratmusrat555@gmail.com>
2024-06-02 21:18:59 -0700

    2ndclass.js

---
+++ 3th class.js
@@ -0,0 +1,67 @@
+//let num = null;
+//console.log(num);
+
+//let num = prompt("what is your name! ");
+//console.log(num--);
+//console.log(num + 5);
+
+//comperison operators
+//== ===
+//let num1 =6;
+//let num2 =6;
+//console.log(num1 ==num2);
+
+//let num1 =6; //data type Number
+//let num2 ='6';//String
+//console.log(num1 == num2);
+
+//let num1 =6; //data type Number
+//let num2 =6;//String
+//console.log(num1 === num2);
+
+//!= , ==!
+//let num1 =6; //data type Number
+//let num2 =7;//String
+//console.log(num1 != num2);
+
+//console.log("5" !==5);
+// < greater than
+// >less than
+//console.log(5 > 7);
+
+//let num = 55;
+//let num2 = 66;
+//console.log(num < num2);
+
+//let num = 155;
+//let num2 = 66;
+//console.log(num > num2);
+
+// <= <==
+//let num = 155;
+//let num2 = 66;
+//console.log(num >= num2);
+
+
+//let num = 155;
+//let num2 = 154;
+//console.log(num <= num2);
+
+
+
+//let age = +prompt("inter your age");
+//if( age >= 18 ){
+//console.log("you can apply cnic");
+
+//}else{
+  // console.log("you are not eligble");
+//}
+
+// let userNum = +prompt("inter your number");
+// let computerNumber = Math.round(Math.random() * 5);
+
+// if(userNum === computerNumber){
+    //console.log("you win");
+// }else{
+    //console.log("you loss !" + computerNumber );
+// }
