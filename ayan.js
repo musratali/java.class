fcd91a2eae9b7d3eea73f284f7bb38b6dc250b90
Musrat Ali <musratmusrat555@gmail.com>
2024-06-02 21:28:53 -0700

    thirdclass.js

---
+++ ayan.js
@@ -0,0 +1,68 @@
+
+// console.log(5 != 7);
+//  let num = 55;
+ // num = 77;
+ // console.log(num);
+
+ //let ice = +prompt("inter your number");
+
+// if(ice === 100){
+//   console.log("ice-cream is corenetto");
+
+// }else if(ice === 500){
+//   console.log(".ice-cream is corenetto family pack 1.5 LTR");
+// }else if(ice === 1000){
+//   console.log("ice-cream is corenetto family pack 2.5 LTR");
+  
+// }else{
+//   console.log("your ice-cream is random");
+// }
+
+// switch (ice) {
+//   case 100:
+//     console.log("ice-cream is corenetto");
+//     break;
+//     case 500:
+//       console.log(".ice-cream is corenetto family pack 1.5 LTR");
+//       break;
+//     case 1000:
+//       console.log(".ice-cream is corenetto family pack 2.5 LTR");
+//       break;
+//       default:
+//          console.log("your ice-cream is random");
+//          break;
+        
+
+// }
+
+// let userNum = prompt("Even number");
+// let compNum = Math.round(Math.random() * 10);
+
+// if(compNum % 2 === 0 && userNum === "even"){
+//   console.log(" you win [ " + compNum);
+// }else if(compNum % 2 !== 0 && userNum === "odd"){
+//    console.log("you win ! " + compNum);
+// }else{
+// console.log("you loss !" + compNum);
+// } 
+// initialistion
+// condition
+// increment
+
+// for(let i = 1; i <= 10 ; i++){
+//   console.log("hello world " + i)
+//
+// for(let i = 1; i <= 100 ; i++){
+//   console.log("hello world " + i)
+//  }
+
+//  for(let i = 1; i <= 20 ; i++){
+//    console.log("2 x " + i + " =" + 4 *i)
+//  }
+
+// let table = 7;
+// for(let i = 1; i <= 10 ; i++){
+//   console.log( table + " x " + i + " =" + table *i)
+//   console.log(Math.round(Math.random() * 10));
+//   console.log("hello world");
+// }
