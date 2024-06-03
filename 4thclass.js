719e537a65f3e4d43233f0bc67c00ad0e74451b7
Musrat Ali <musratmusrat555@gmail.com>
2024-06-02 21:33:13 -0700

    fourclass.js

---
+++ 4thclass.js
@@ -0,0 +1,71 @@
+
+// function
+
+
+// function greet () {
+//   console.log("hello student");
+// }
+// greet() // function calling
+// greet()
+// greet()
+// greet()
+// greet()
+
+// function employee (file){// parameter
+//   console.log("working on file " + file);
+// }
+
+// employee("abdullah")// argument
+// employee("ali")
+// employee("wasif")
+// employee("hussan")
+
+
+//  function employee (file){// parameter
+//    return "working on file " + file
+//  }
+
+//  let data = employee(7);
+//  console.log(data );
+
+// function add = (num , num2, num3){
+//   return num1 + num2 + num3;
+// }
+
+// let data1 = add(22, 22, 33)
+
+// let data2 = add(222, 22, 333);
+// console.log(data1, data2);/
+
+// // closure ==== scope
+// let nam2 = " musrat sadaf"; 
+
+// let abc = 123 ; // global scope
+// function print ( ){
+//   let abc = 123 ; // local scope
+//   console.log(abc);
+//}
+
+
+
+// // console.log(abc);
+// // print()
+// //let name = "musrat";
+//  function print () {
+// //   console.log(nam2);
+  
+//   //let nam2 = " sadaf"; //global scope
+//   // console.log(name + nam2);
+// let nam2 = "sadaf 123"
+//   return function () {   // anonymous function
+//     //let nam2 = " sadaf khan";    //local scope 
+//   console.log( nam2 );  
+  
+
+//   }
+// }
+// let innerfunction = print();
+// innerfunction()
+
+// // console.log( nam2);
+// // print()
