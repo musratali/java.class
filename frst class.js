b0e2c796e88fed03f8dfc3e672f59a7b876f560e
Musrat Ali <musratmusrat555@gmail.com>
2024-06-02 21:10:13 -0700

    frst class

---
+++ frst class.js
@@ -0,0 +1,98 @@
+//let num=77;
+//console.log(num);
+
+//let num=33;
+//console.log(num);
+
+//let num=44;
+//let num=66;
+//console.log(num);
+
+//let firstName = "ali"
+//let lastname = "ahmed";
+
+//console.log(firstName + ""+ lastname);
+
+// number + number addition
+// num + string concatenation
+// string + number concatenation
+
+//let num = "8";
+//let num2 = 9;
+//console.log(num + num2)
+
+//let email = "asd@gamil.com"
+//let name = "abdullah"
+//let account = 7893783267;
+//let etc
+//console.table([email , name , account, etc])
+
+//math operators = + -  * / divided % modules
+
+//let num = 66;
+//let num2 = 1;
+//console.log(num + num2);
+
+
+//let num = "66";
+//let num2 = 1;
+//console.log(num + num2);
+
+//let data = 100;
+//let data2 = 5;
+//console.log(data * data2);
+
+//let data = 100;
+//let data2 = 5;
+//console.log(data / data2);
+
+//let data = 10;
+//let data2 =3;
+//let add = data % data2;
+//console.log(add);
+
+//let data = 11;
+//let data2 =3;
+//let add = data % data2;
+//console.log(add);
+
+
+//let name = prompt("what is your name............");
+
+//
+//document.write(name)
+
+//let name = prompt("what is your number..............");
+//console.log(name);
+
+//let rendomNumber1 = Math.random();
+//console.log(rendomNumber1)
+
+//let rendomNumber = Math.random();
+//rendomNumber = rendomNumber * 10;
+//let rounded = Math.round(rendomNumber)
+//console.log(rounded)
+//BODMAS = BARKET OPEN DIVIDED MULPLY ADD SUBTRA
+//let num = 4 + 6 * (5 + 5) ;
+//console.log(num);
+
+
+//let x = 33;
+//console.log(x);
+//console.log(typeof string (x));
+
+//let name = null;
+//console.log(name);
+
+/////Boolean
+//let x = 33;
+//let y = false;
+
+//console.log(typeof y);
+
+//console.log(x + y)
+
+
+//let x = 33;
+//let y = true;
+//console.log(x + 12 + y);
