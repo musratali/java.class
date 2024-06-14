261ff51d16255e04645293278a5415eadaa04c6e
Musrat Ali <musratmusrat555@gmail.com>
2024-06-14 11:08:50 +0500

    10th class.js

---
+++ 10th.js
@@ -0,0 +1,61 @@
+//synhronous vs asynchronous
+
+
+// alert("hello students");
+// console.log('hello japs');
+
+
+// asynchronous
+// setTimeout(function(){
+//     console.log("hello javascript")
+// }, 5000)
+
+// console.log("hello pakistan");
+
+// callback function
+// function runaftsec (cbs){
+//      setTimeout(function (){
+//      console.log('abc ');
+//      console.log(typeof cbs);
+
+//      }, 1000)
+// }
+// function tobeexcute (){
+//      console.log("done");
+// }
+// runaftsec(tobeexcute )
+
+// function runtime (abc, val, c  ){
+//      setTimeout(function(){
+//           let data = 5 + val + c
+//           abc(data)
+//      },1000)
+// }
+
+// function tobeexcute (datatoget ){
+//      console.log(" done ", datatoget );
+// }
+
+// runtime ( function tobeexcute (datatoget ){
+//      console.log(" done ", datatoget );
+// }, 99, 1 )
+
+// function sum (a, b, c, vals ){
+//      console.log(a + b + c + vals);
+//      console.log(vals);
+// }
+
+// sum(2,9,10,100)
+
+
+// Promise
+
+// function runtime (val){
+     // return new Promise((resolve) => {
+     //      setTimeout(function(){
+     //           let data = 5 + Val
+     //           resolve(data)
+     //      }, 1000 )
+     // })
+
+// }
