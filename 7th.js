3cde1960ecabab3693607b1e6d74310e4e673188
Musrat Ali <musratmusrat555@gmail.com>
2024-06-14 10:53:23 +0500

    7th class.js

---
+++ 7th.js
@@ -0,0 +1,93 @@
+
+// let person1 = {
+
+//    name : "wasif",
+//    cnic : 87563437674,
+//    language : "sindhi",
+//    class : 'javascript' ,
+//    speck : function (students){
+//       console.log(students + this.name );
+
+//    }
+
+// }
+// console.log(person1);
+// let person2 = {
+
+//    name : "abdullah", //property
+//    cnic : 87563437674, // key
+//    language : "sindhi",
+//    class : 'javascript' ,
+//    speck : function (students){
+//       console.log(students + this.name );
+
+//    }
+
+// }
+// person1.speck("hello i m ");
+// person2.speck("hello i m ")
+
+
+
+// let person1 = {
+
+//    name : "wasif",
+//    cnic : 87563437674,
+//    language : "sindhi",
+//    class : 'javascript' ,
+//    speck : function (students){
+//       console.log(students + this.name );
+
+//    }
+
+// }
+// let person2 = {
+
+//    name : "abdullah", //property
+//    cnic : 87563437674, // key
+//    language : "sindhi",
+//    class : 'javascript' ,
+//    speck : function (students){
+//       console.log(students + this.name );
+
+//    }
+// }
+
+// let students = [
+//    person1 , person2
+// ];
+// console.log(students);
+
+
+// function students (name1 , cnic1 , language1 , javascript1){
+//   return{
+
+//      name : name1,
+//      cnic : cnic1,
+//      language : language1,
+//      class : javascript1
+
+//   }
+
+// }
+
+// console.log(students("musrat", 6788, "sindhi" ,"html"));
+// let students1 = [students("ali", 4333333, "sindhi", "html")];
+// let students2 = [students( )];
+// console.log(students1);
+
+
+// function students (){
+//     return{
+   
+
+//          name : prompt("inter your name"),
+//          cnic : +prompt("inter your card num"),
+//          language : prompt("inter your language"),
+//          class : prompt("inter your class"),
+        
+//          }
+// }
+
+// let students1 = [students("ali", 4333333, "sindhi", "html" )];
+// console.log(students1);
