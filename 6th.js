90f10df152313115a1e8df5a2efc0be63abfa40f
Musrat Ali <musratmusrat555@gmail.com>
2024-06-14 10:47:12 +0500

    six class

---
+++ 6th.js
@@ -0,0 +1,68 @@
+
+
+// console.log("hello world");
+
+// let num = [1,234,5,6,7,8,9,900,0];
+// let num2 = num.slice(2,6);
+// console.log(num);
+// console.log(num2);
+
+// let name = [];
+// for(let i = 1; i < 100; i++){
+//     name.push("hello")
+// }
+// console.log(name);
+
+// let name = ["zaheer", "muhammad-ali", "wasif", "ali","noman"];
+
+// for (let elem in name){
+//     console.log(elem);
+// }
+
+// let name = ["zaheer", "muhammad-ali", "wasif", "ali","noman"];
+
+// for (let elem of name){
+//     console.log(elem);
+// }
+
+//  let arr = ["zaheer", "muhammad-ali", "wasif", "ali","noman"];
+ 
+//  arr.forEach(function(val, ind){
+// console.log(val , ind);
+//  })
+
+//array filter
+
+// let filterarr = [1,2,3,4,5,6,7,8];
+
+// let arr = filterarr.filter(function(val){
+
+//     if(val% 2 != 0){
+//         return true;
+//     }else{
+//         return false
+//     }
+
+// })
+// console.log(arr);
+// console.log(filterarr);
+
+// let arr = [109,11, 1, 3, 0,4,67,8,9,9,0,0,9,8,7,1,1,1,1,233,4,5,6];
+// console.log(arr);
+// let arrsort = arr.sort(function(a,b){
+//     if(a > b ){
+//         return 1
+//     }else{
+//         return -1
+//     }
+// })
+ 
+// console.log(arrsort);
+
+
+//  let arr = ["ali "," " ,"ahmed " , ];
+
+//  let a = arr.reduce(function(preval, currval){
+//     return preval + currval;
+//  })
+// console.log(a);
